94ce08b8f38b8b4e3450224c3ba734e21e662fdc
Sergio Burdisso <sergio.burdisso@gmail.com>
2017-03-13 12:25:26 -0300

    flake8 style, except for public methods, which are camelCase

--- solidwebpush/__init__.py
+++ solidwebpush/__init__.py
@@ -24,7 +24,7 @@ __license__ = 'MIT'
 
 
 def __doc_from__(docfunc):
-    """ a @decorator """
+    """A @decorator."""
     def __wrapper__(func):
         func.__name__ = docfunc.__name__
         func.__doc__ = docfunc.__doc__
@@ -33,20 +33,20 @@ def __doc_from__(docfunc):
 
 
 def __database__(func):
-    """ a @decorator """
+    """A @decorator."""
     @__doc_from__(func)
     def __init_database__(*args, **kwargs):
         s = args[0]
 
-        if not s.__dbConn__:
-            s.__dbConn__ = db_connect(s.__dbName__)
-            s.__dbConn__.row_factory = lambda cursor, row: \
+        if not s.__db_conn__:
+            s.__db_conn__ = db_connect(s.__db_name__)
+            s.__db_conn__.row_factory = lambda cursor, row: \
                 dict((
                     (col[0], row[idx])
                     for idx, col
                     in enumerate(cursor.description)
                 ))
-            s.__db__ = s.__dbConn__.cursor()
+            s.__db__ = s.__db_conn__.cursor()
 
             try:
                 s.__db__.executescript('''
@@ -56,14 +56,14 @@ def __database__(func):
                         group_id INT NOT NULL
                     );
                 ''')
-                s.__dbConn__.commit()
+                s.__db_conn__.commit()
             except:
                 pass
 
             result = func(*args, **kwargs)
 
-            s.__dbConn__.close()
-            s.__dbConn__ = None
+            s.__db_conn__.close()
+            s.__db_conn__ = None
         else:
             result = func(*args, **kwargs)
 
@@ -77,8 +77,11 @@ def __database__(func):
 
 class Pusher:
     """
+    Pusher objects allows you to integrate Web Push Notifications
+    into your project.
+
     Instantiate this class to integrate Web Push Notifications
-    to your server. Objects of this class will create your public
+    into your server. Objects of this class will create your public
     and private key, track your subscriptions, notify your clients,
     and do all the required work for you.
 
@@ -109,31 +112,32 @@ class Pusher:
     (for more "toy" examples visit
      https://github.com/sergioburdisso/solidwebpush/tree/master/examples)
     """
+
     __vapid__ = None
     __verbose__ = False
 
-    __dbName__ = None
-    __dbConn__ = None
+    __db_name__ = None
+    __db_conn__ = None
     __db__ = None
     __pool__ = None
 
     __RE_URL__ = r"(https?://(?:[\w-]+\.)*[\w-]+(?::\d+)?)(?:/.*)?"
 
-    def __init__(self, dbName="subscriptors.db", verbose=False):
+    def __init__(self, db_name="subscriptors.db", verbose=False):
         """
-        Class constructor
+        Class constructor.
 
-        :param dbName: The [optional] name ("subscriptors.db" by default) of
+        :param db_name: The [optional] name ("subscriptors.db" by default) of
                        the file in which subscriptions will be stored in.
                        This is only required if methods like
                        ``newSubscription`` will be used.
-        :type dbName: str
+        :type db_name: str
         :param verbose: An optional value, to enabled or disabled the "verbose
                         mode" (False by default)
         :type verbose: bool
         """
         self.__verbose__ = verbose
-        self.__dbName__ = dbName
+        self.__db_name__ = db_name
 
         if not os.path.exists('private_key.pem'):
             self.__print__("No private_key.pem file found")
@@ -150,44 +154,49 @@ class Pusher:
             self.__print__("PublicKey: %s" % self.getB64PublicKey())
 
     def __getstate__(self):
+        """Class state getter."""
         self_dict = self.__dict__.copy()
         del self_dict['__pool__']
         return self_dict
 
     def __call__(self, subscription, data):
+        """Class instances callable."""
         self.__send__(subscription, data)
 
     def __print__(self, msg):
+        """Verbose print wrapper."""
         print "[ SolidWebPush ] %s" % msg
 
     def __b64rpad__(self, b64str):
+        """Base64 right (=)padding."""
         return b64str + b"===="[:len(b64str) % 4]
 
-    def __encrypt__(self, userPublicKey, userAuth, payload):
-        userPublicKey = userPublicKey.encode("utf8")
-        rawUserPublicKey = base64.urlsafe_b64decode(
-            self.__b64rpad__(userPublicKey)
+    def __encrypt__(self, user_publickey, user_auth, payload):
+        """Encrypt the given payload."""
+        user_publickey = user_publickey.encode("utf8")
+        raw_user_publickey = base64.urlsafe_b64decode(
+            self.__b64rpad__(user_publickey)
         )
 
-        userAuth = userAuth.encode("utf8")
-        rawUserAuth = base64.urlsafe_b64decode(self.__b64rpad__(userAuth))
+        user_auth = user_auth.encode("utf8")
+        raw_user_auth = base64.urlsafe_b64decode(self.__b64rpad__(user_auth))
 
         salt = os.urandom(16)
 
         curve = pyelliptic.ECC(curve="prime256v1")
-        curveId = base64.urlsafe_b64encode(curve.get_pubkey()[1:])
+        curve_id = base64.urlsafe_b64encode(curve.get_pubkey()[1:])
 
         payload = payload.encode("utf8")
 
-        http_ece.keys[curveId] = curve
-        http_ece.labels[curveId] = "P-256"
+        http_ece.keys[curve_id] = curve
+        http_ece.labels[curve_id] = "P-256"
 
         encrypted = http_ece.encrypt(
             payload,
-            keyid=curveId,
-            dh=rawUserPublicKey,
+            keyid=curve_id,
+            dh=raw_user_publickey,
             salt=salt,
-            authSecret=rawUserAuth
+            authSecret=raw_user_auth
         )
 
         return {
@@ -197,6 +206,7 @@ class Pusher:
         }
 
     def __send__(self, subscription, data):
+        """Encrypt and send the data to the Message Server."""
         subscription = json.loads(subscription)
 
         if type(data) == dict:
@@ -213,13 +223,13 @@ class Pusher:
             data
         )
 
-        jwtpayload = {
+        jwt_payload = {
             "aud": base_url,
             "exp": int(time.time()) + 60 * 60 * 12,
             "sub": "mailto:admin@yamanouchihnos.com"
         }
 
-        headers = self.__vapid__.sign(jwtpayload)
+        headers = self.__vapid__.sign(jwt_payload)
         headers["TTL"] = str(43200)
         headers["Content-Type"] = "application/octet-stream"
         headers['Content-Encoding'] = 'aesgcm'
@@ -244,7 +254,9 @@ class Pusher:
 
     def setVerbose(self, value):
         """
-        Enables and disables the verbose mode (disabled by default).
+        Verbose mode.
+
+        Enable and disable the verbose mode (disabled by default).
         When verbose mode is active, some internal messages are going
         to be displayed, as well as the responses from the Message Server.
 
@@ -255,6 +267,8 @@ class Pusher:
 
     def getPublicKey(self):
         """
+        Raw public key getter.
+
         :returns: the raw public key
         :rtype: str
         """
@@ -262,6 +276,8 @@ class Pusher:
 
     def getPrivateKey(self):
         """
+        Raw private key getter.
+
         (probably you won't care about private key at all)
 
         :returns: the raw private key
@@ -271,6 +287,8 @@ class Pusher:
 
     def getB64PublicKey(self):
         """
+        Base64 public key getter.
+
         Returns the string you're going to use when subscribing your
         serviceWorker.
         (as long as you're planning to decode it using JavaScript's
@@ -283,6 +301,8 @@ class Pusher:
 
     def getB64PrivateKey(self):
         """
+        Base64 private key getter.
+
         (probably you won't care about private key at all)
 
         :returns: Base64-encoded version of the private key
@@ -292,6 +312,8 @@ class Pusher:
 
     def getUrlB64PublicKey(self):
         """
+        Url-Safe Base64 public key getter.
+
         This is the string you're going to use when subscribing your
         serviceWorker.
         (so long as you're planning to decode it using a function like
@@ -305,6 +327,8 @@ class Pusher:
 
     def getUrlB64PrivateKey(self):
         """
+        Url-Safe Base64 private key getter.
+
         (probably you won't care about private key at all)
 
         :returns: URLSafe-Base64-encoded version of the private key
@@ -314,6 +338,8 @@ class Pusher:
 
     def sendNotification(self, subscription, data, nonblocking=False):
         """
+        Send the data to the Message Server.
+
         Pushes a notification carrying ``data`` to
         the client associated with the ``subscription`` object.
         If ``nonblocking`` is False, the program won't block waiting
@@ -342,6 +368,8 @@ class Pusher:
     def sendNotificationToAll(
             self, subscriptions, data, nonblocking=False, processes=None):
         """
+        Send the data to the Message Server.
+
         Pushes a notification carrying ``data`` to
         each of the clients associated with the list of ``subscriptions``.
         If ``nonblocking`` is False, the program won't block waiting
@@ -382,7 +410,9 @@ class Pusher:
 
     def wait(self):
         """
-        Blocks the program and waits for all the notifications to be sent,
+        Wait for all the messages to be completely sent.
+
+        Block the program and wait for all the notifications to be sent,
         before continuing.
         This only works if there exist a previous call to a method
         with the ``nonblocking`` parameter set to ``False``,
@@ -401,19 +431,21 @@ class Pusher:
         self.__pool__ = None
 
     @__database__
-    def newSubscription(self, idSession, subscription, idGroup=0):
+    def newSubscription(self, session_id, subscription, group_id=0):
         """
-        newSubscription(idSession, subscription, idGroup=0)
+        newSubscription(session_id, subscription, group_id=0)
+        Permanently subscribe a client.
+
         Subscribes the client by permanently storing its ``subscription`` and
-        group id (``idGroup``).
+        group id (``group_id``).
         This will allow you to push notifications using the
-        client id (``idSession``) instead of its ``subscription`` object.
+        client id (``session_id``) instead of its ``subscription`` object.
 
         Groups help you organize subscribers. For instance, suppose you
         want to notify Bob by sending a notification to all of his devices.
         If you previously subscribed each one of his devices to the same group
-        let's say 13, then calling notifyall with 13 will push notifications to
-         all of them:
+        let's say 13, then calling notifyAll with 13 will push notifications to
+        all of them:
 
             >>> BobsGroup = 13
             >>> ...
@@ -437,60 +469,64 @@ class Pusher:
             >>> ...
             >>> pusher.notifyAll(BobsGroup)
 
-        :param idSession: The client's identification
+        :param session_id: The client's identification
                           (e.g. a cookie or other session token)
-        :type idSession: str
+        :type session_id: str
         :param subscription: The client's subscription JSON object
         :type subscription: str
-        :param idGroup: an optional Group ID value (0 by default)
-        :type idGroup: int
+        :param group_id: an optional Group ID value (0 by default)
+        :type group_id: int
         """
-        if not self.getSubscription(idSession):
-            oldIdSession = self.getIdSession(subscription)
-            if oldIdSession:
-                self.removeSubscription(oldIdSession)
+        if not self.getSubscription(session_id):
+            old_session_id = self.getIdSession(subscription)
+            if old_session_id:
+                self.removeSubscription(old_session_id)
             self.__db__.execute(
                 "INSERT INTO subscriptors (session_id, subscription, group_id)"
                 " VALUES (?,?,?)",
-                (idSession, subscription, idGroup)
+                (session_id, subscription, group_id)
             )
         else:
             self.__db__.execute(
                 "UPDATE subscriptors SET subscription=?, group_id=? WHERE"
                 " session_id=?",
-                (subscription, idGroup, idSession,)
+                (subscription, group_id, session_id,)
             )
-        self.__dbConn__.commit()
+        self.__db_conn__.commit()
 
     @__database__
-    def removeSubscription(self, idSession):
+    def removeSubscription(self, session_id):
         """
-        removeSubscription(idSession)
+        removeSubscription(session_id)
+        Permanently unsubscribes a client.
+
         Unsubscribes the client by permanently removing its ``subscription``
         and group id.
 
-        :param idSession: The client's identification (e.g. a cookie or other
+        :param session_id: The client's identification (e.g. a cookie or other
                           session token)
-        :type idSession: str
+        :type session_id: str
         """
         self.__db__.execute(
             "DELETE FROM subscriptors WHERE session_id = ?",
-            (idSession,)
+            (session_id,)
         )
-        self.__dbConn__.commit()
+        self.__db_conn__.commit()
 
     @__database__
-    def notify(self, idSession, data, nonblocking=False):
+    def notify(self, session_id, data, nonblocking=False):
         """
-        notify(idSession, data)
+        notify(session_id, data, nonblocking=False)
+        Notify a given client.
+
         Pushes a notification carrying ``data`` to the client associated with
-        the ``idSession``.
-        ``idSession`` is the value passed to the ``newSubscription`` method
+        the ``session_id``.
+        ``session_id`` is the value passed to the ``newSubscription`` method
         when storing the client's subscription object.
 
-        :param idSession: The client's identification (e.g. a cookie or other
+        :param session_id: The client's identification (e.g. a cookie or other
                           session token)
-        :type idSession: str
+        :type session_id: str
         :param data: A string or a dict object to be sent.
                      The dict will be automatically converted into a JSON
                      string before being sent.
@@ -502,18 +538,20 @@ class Pusher:
         :type nonblocking: bool
         """
         self.sendNotification(
-            self.getSubscription(idSession),
+            self.getSubscription(session_id),
             data,
             nonblocking=nonblocking
         )
 
     @__database__
-    def notifyAll(self, data, idGroup=None, exceptList=[], nonblocking=False):
+    def notifyAll(self, data, group_id=None, exceptions=[], nonblocking=False):
         """
-        notifyAll(data, idGroup=None, exceptList=[])
-        When no ``idGroup`` is given, notify all subscribers (except for those
-        in ``exceptList``). Otherwise, it only notifies all members of the
-        ``idGroup`` group (except for those in ``exceptList``).
+        notifyAll(data, group_id=None, exceptions=[], nonblocking=False)
+        Notify a group of clients.
+
+        When no ``group_id`` is given, notify all subscribers (except for those
+        in ``exceptions``). Otherwise, it only notifies all members of the
+        ``group_id`` group (except for those in ``exceptions``).
 
         :param data: A string or a dict object to be sent.
                      The dict will be automatically converted into a JSON
@@ -521,15 +559,18 @@ class Pusher:
                      An example of a dict object would be:
                      ``{"title": "hey Bob!", "body": "you rock"}``
         :type data: str or dict
-        :param idGroup: an optional Group ID value (0 by default)
-        :type idGroup: int
-        :param exceptList: The list of sessions ids to be excluded.
-        :type exceptList: list
+        :param group_id: an optional Group ID value (0 by default)
+        :type group_id: int
+        :param exceptions: The list of sessions ids to be excluded.
+        :type exceptions: list
         :param nonblocking: Whether to block the caller until this method
                             finishes running or not.
         :type nonblocking: bool
         """
-        condition = " WHERE group_id=" + idGroup if idGroup is not None else ""
+        if group_id is not None:
+            condition = " WHERE group_id=" + group_id
+        else:
+            condition = ""
 
         self.sendNotificationToAll(
             [
@@ -537,7 +578,7 @@ class Pusher:
                 for row in self.__db__.execute(
                     "SELECT * FROM subscriptors" + condition
                 ).fetchall()
-                if row["session_id"] not in exceptList
+                if row["session_id"] not in exceptions
             ],
             data,
             nonblocking=nonblocking
@@ -561,36 +602,36 @@ class Pusher:
         return res.values()[0] if res else None
 
     @__database__
-    def getSubscription(self, idSession):
+    def getSubscription(self, session_id):
         """
-        getSubscription(idSession)
+        getSubscription(session_id)
         Given a session id returns the subscription object associated with it.
 
-        :param idSession: A session id
-        :type idSession: str
+        :param session_id: A session id
+        :type session_id: str
         :returns: The client's subscription JSON object associated with
                   the session id.
         :rtype: str
         """
         res = self.__db__.execute(
             "SELECT subscription FROM subscriptors WHERE session_id=?",
-            (idSession,)
+            (session_id,)
         ).fetchone()
         return res.values()[0] if res else None
 
     @__database__
-    def getGroupId(self, idSession):
+    def getGroupId(self, session_id):
         """
-        getGroupId(idSession)
+        getGroupId(session_id)
         Given a session id returns the group id it belongs to.
 
-        :param idSession: A session id
-        :type idSession: str
+        :param session_id: A session id
+        :type session_id: str
         :returns: a group id value
         :rtype: int
         """
         res = self.__db__.execute(
             "SELECT group_id FROM subscriptors WHERE session_id=?",
-            (idSession,)
+            (session_id,)
         ).fetchone()
         return res.values()[0] if res else None
